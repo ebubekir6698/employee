40d7996f647c312f008a8dbb0bf8c258d5f88779
Ebubekir Kocaman <ayyildiz6698@gmail.com>
2022-03-23 17:47:05 +0000

    Small fix

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -4,36 +4,18 @@ import './Level.css';
 
 import Employee from '../Employee/Employee';
 
-function Level(props) {
-    const [employees, setEmployees] = useState([]);
-
-    useEffect(() => {
-        const employeeList = [];
-
-        for(let i = 0, len = props.employees.length; i < len; i++){
-            const person = props.employees[i];
-
-            console.log(person);
-    
-            const employee = (
-                <Employee
-                    key={person.id}
-                    firstName={person.firstName}
-                    lastName={person.lastName}
-                    role={person.role}
-                    picture={person.picture}
-                />
-            );
-    
-            employeeList.push(employee);
-        }
-        
-        setEmployees(employeeList);
-    }, []);
-
+function Level({employees}) {
     return (
         <div className='Level'>
-            {employees}
+            {employees.map(employee => (
+                <Employee
+                key={employee.id}
+                firstName={employee.firstName}
+                lastName={employee.lastName}
+                role={employee.role}
+                picture={employee.picture}
+            />
+            ))}
         </div>
     );
 }
